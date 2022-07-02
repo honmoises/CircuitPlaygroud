33f20ec6ab0a0054d775d7d3ca181402cfe2fd77
Moises <honmoises@gmail.com>
2022-07-02 16:21:45 -0600

    Update semana7_practica3.ino

--- semana7_practica3.ino
+++ semana7_practica3.ino
@@ -19,18 +19,26 @@ CircuitPlayground.clearPixels();
 if (CircuitPlayground.leftButton()) {
    Serial.println("Cartago Campeón");
    CircuitPlayground.setPixelColor(5,50,50,196);
+   delay(100);
    CircuitPlayground.setPixelColor(6,50,50,196);
+   delay(100);
    CircuitPlayground.setPixelColor(7,50,50,196);
+   delay(100);
    CircuitPlayground.setPixelColor(8,50,50,196);
+   delay(100);
    CircuitPlayground.setPixelColor(9,50,50,196);
    
    delay(200);
 } else if (CircuitPlayground.rightButton()) {
    Serial.println("Liga Campeón");
    CircuitPlayground.setPixelColor(0,204,0,0);
+   delay(100);
    CircuitPlayground.setPixelColor(1,204,0,0);
+   delay(100);
    CircuitPlayground.setPixelColor(2,204,0,0);
+   delay(100);
    CircuitPlayground.setPixelColor(3,204,0,0);
+   delay(100);
    CircuitPlayground.setPixelColor(4,204,0,0);
    delay(200);
 
